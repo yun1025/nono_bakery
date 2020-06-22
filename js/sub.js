bf88fda2dc72d5bae1f99b74e33b91a7024b5834
yun <brown1025@naver.com>
2020-06-22 12:42:24 +0900

    Feat: 영양정보 검색 기능 추가 중(미완성)

--- js/sub.js
+++ js/sub.js
@@ -10,3 +10,46 @@ $(function() {
 		}
 	});	
 })
+
+/* 영양정보 */
+function nutrition() {
+	var input_group = $(".group").val();			//분류 기본값
+	var group = "";
+	var input_product = $("#product").val();		//제품명 기본값
+	var product =""
+	
+	//$(".loading").height( $(".nutrition_list tbody").height() ).show();
+	
+	/* 분류 */
+	if(input_group != "none") {
+		for(i=1; i < $(".nutrition_list tr").length; i++) {
+			group = $(".nutrition_list tr:eq("+ i +") td:eq(0)").children("span").attr("class");
+			
+			if(input_group == group) {
+				$(".nutrition_list tr:eq("+ i +")").removeClass("hide");				
+			}else if(input_group != group) {
+				$(".nutrition_list tr:eq("+ i +")").addClass("hide");
+			}
+		}
+	}else {
+		$(".nutrition_list tr").removeClass("hide");
+	}
+	
+	/* 제품명 */	
+	if(input_product != "") {
+		for(i=1; i < $(".nutrition_list tr").length; i++) {
+			product = $(".nutrition_list tr:eq("+ i +") td:eq(1)").html();
+			
+			if(product.indexOf(input_product) != -1) {
+				$(".nutrition_list tr:eq("+ i +")").removeClass("hide");
+			}else if(product.indexOf(input_product) == -1) {
+				$(".nutrition_list tr:eq("+ i +")").addClass("hide");
+			}
+		}
+	}
+	
+	setTimeout(function() {
+		$(".nutrition_list tr.hide td").fadeOut();
+		$(".nutrition_list tr:not(.hide) td").fadeIn();
+	}, 2000)	
+}
